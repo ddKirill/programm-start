398acd0e85a535a8e87d4e8c3fc156122c524dca
ddKirill <cfaleev@yandex.ru>
2020-11-19 02:07:50 +0300

    New

---
+++ src/com/company/task1/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.company.task1;
+
+public class BinarySearch {
+
+
+    public boolean search(int[] array, int find) {
+
+        boolean result = false;
+
+        int low = 0;
+        int high = array.length - 1;
+
+
+        while (!result) {
+
+            if (low > high) {
+                break;
+            }
+            //нашел индекс среднего элемента
+            int midIndex = (low + high) / 2;
+            //число среднего элемента
+            int value = array[midIndex];
+            //рассматриваем левую часть массива
+            if (midIndex > find) {
+                high = midIndex - 1;
+                //рассматриваем правую часть массива
+            } else if (midIndex < find) {
+                low = midIndex + 1;
+            } else {
+                result = true;
+            }
+            return result;
+
+        }
+    }
