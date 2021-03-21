f555d1c3c3da9296454f7ee84432d7c8f6dacff8
ddKirill <cfaleev@yandex.ru>
2021-03-21 21:19:22 +0300

    !

---
+++ src/com/company/task2/SelectSort.java
@@ -0,0 +1,24 @@
+package com.company.task2;
+
+public class SelectSort {
+
+    public static void selectIntSort(int array[]){
+
+        int min, tmp;
+
+        for (int i = 0; i < array.length; i++){
+            min = i;
+            for (int scan = i + 1; scan < array.length; scan ++){
+                if (array[scan] < array [min]){
+                    min = scan;
+                }
+                tmp = array[min];
+                array[min] = array[i];
+                array[i] = tmp;
+            }
+        }
+
+    }
+}
+
+
